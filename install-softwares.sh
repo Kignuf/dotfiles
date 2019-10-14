3f7ff55ccf8f9077bf65941a293e1d1a57ef54d7
Kevin Roulleau <kevinroulleau0@gmail.com>
2019-10-14 22:22:46 +0200

    Add more apps to be installed

--- install-softwares.sh
+++ install-softwares.sh
@@ -4,5 +4,10 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install softwares
-brew install git tig bash-completion node hub &&\
-brew cask install vscode
+brew install git tig bash-completion node hub java &&\
+brew cask install iterm2 vscode 1clipboard chrome firefox spotify flux dashlane android-studio appium discord slack docker
+
+# Fira code
+# https://github.com/tonsky/FiraCode/wiki/Installing
+brew tap homebrew/cask-fonts &&\
+brew cask install font-fira-code
