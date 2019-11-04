91c0cc196ba9fcdb7af56ec50055a0a07cc861e6
Kevin Roulleau <kevinroulleau0@gmail.com>
2019-11-04 15:23:17 +0100

    Add terraform, ansible and awscli

--- install-softwares.sh
+++ install-softwares.sh
@@ -4,7 +4,7 @@
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install softwares
-brew install git tig bash-completion node hub java &&\
+brew install git tig bash-completion node hub java ansible terraform awscli &&\
 brew cask install iterm2 vscode 1clipboard chrome firefox spotify flux dashlane android-studio appium discord slack docker
 
 # Fira code
