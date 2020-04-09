397bfdbe7a2dad0a131d941eea7a67247655a059
Kevin Roulleau <kevinroulleau0@gmail.com>
2020-04-09 12:19:01 +0200

    Fix install softwares

--- install-softwares.sh
+++ install-softwares.sh
@@ -11,9 +11,12 @@ brew cask install adoptopenjdk8
 # Install softwares
 echo "Installing other softwares"
 brew install git tig bash-completion node hub ansible terraform awscli gpg jq the_silver_searcher &&\
-brew cask install iterm2 visual-studio-code google-chrome firefox spotify flux android-studio appium discord slack docker skype whatsapp
+brew cask install iterm2 visual-studio-code google-chrome firefox spotify flux android-studio appium discord slack docker skype whatsapp zoomus postman vlc
 
 # Fira code
 # https://github.com/tonsky/FiraCode/wiki/Installing
 brew tap homebrew/cask-fonts &&\
 brew cask install font-fira-code
+
+# Iterm2 shell integration
+curl -L https://iterm2.com/shell_integration/install_shell_integration_and_utilities.sh | bash
